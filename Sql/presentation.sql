048ed19055964fc11c38a41d0d4acd56ecfcce84
cleav31 <100249643+cleav31@users.noreply.github.com>
2026-04-20 11:04:27 -0400

    Power BI Views
    Views were created to narrow the focus for utilization in Power BI for visualization.

---
+++ Sql/presentation.sql
@@ -0,0 +1,95 @@
+# Executive KPI Overview
+CREATE VIEW vw_kpi_summary AS
+SELECT
+	COUNT(DISTINCT o.order_id) AS total_orders,
+    SUM(oi.quantity * oi.unit_price) AS total_revenue,
+    AVG(DATEDIFF(s.ship_date, o.order_date)) AS avg_fullfillment_days,
+    AVG(CASE
+		WHEN s.delivery_date <= o.required_date THEN 1
+        ELSE 0
+	END) AS on_time_delivery_rate,
+    AVG(q.defective_units/(q.defective_units + q.passed_units))
+		AS defect_rate
+FROM orders o
+JOIN shipments s ON o.order_id = s.order_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN production_batches pb ON oi.product_id = pb.product_id
+JOIN quality_inspections q ON pb.batch_id = q.batch_id;
+
+# Operations(Orders & Logistics)
+CREATE VIEW vw_operations_orders AS
+SELECT
+	o.order_id,
+    o.order_date,
+    o.required_date,
+    s.ship_date,
+    s.delivery_date,
+    s.warehouse_id,
+    DATEDIFF(s.ship_date, o.order_date) AS fullfillment_days,
+    DATEDIFF(s.delivery_date, s.ship_date) AS delivery_days,
+    DATEDIFF(s.delivery_date, o.required_date) AS delivery_delay,
+    CASE
+		WHEN s.delivery_date <= o.required_date THEN 'On Time'
+        ELSE 'Late'
+	END AS delivery_status
+FROM orders o
+JOIN shipments s ON o.order_id = s.order_id;
+
+CREATE VIEW vw_warehouse_performance AS
+SELECT
+	warehouse_id,
+    COUNT(*) AS total_shipments,
+    AVG(DATEDIFF(delivery_date, ship_date)) AS avg_delivery_days,
+    AVG(CASE
+		WHEN delivery_date <= required_date THEN 1
+        ELSE 0
+	END) AS on_time_rate
+FROM shipments s
+JOIN orders o ON s.order_id = o.order_id
+GROUP BY warehouse_id;
+
+# Quality Analysis
+CREATE VIEW vw_quality_batches AS
+SELECT
+	pb.batch_id,
+    pb.product_id,
+    pb.supplier_id,
+    pb.production_date,
+    
+    q.inspection_date,
+    q.defective_units,
+    q.passed_units,
+    (q.defective_units/(q.defective_units + q.passed_units)) AS defect_rate
+FROM production_batches pb
+JOIN quality_inspections q ON pb.batch_id = q.batch_id;
+
+CREATE VIEW vw_supplier_quality AS
+SELECT
+	supplier_id,
+    COUNT(*) AS total_batches,
+    AVG(q.defective_units/(q.defective_units + q.passed_units)) AS avg_defect_rate
+FROM production_batches pb
+JOIN quality_inspections q ON q.batch_id = pb.batch_id
+GROUP BY supplier_id;
+
+# Sales/ Product Performance
+CREATE VIEW vw_product_sales AS
+SELECT
+	p.product_id,
+    SUM(oi.quantity) AS total_units_sold,
+    SUM(oi.quantity * oi.unit_price) AS total_revenue
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY p.product_id;
+
+CREATE VIEW vw_customer_sales AS
+SELECT
+	o.customer_id,
+    c.customer_name,
+    COUNT(DISTINCT o.order_id) AS total_orders,
+    SUM(oi.quantity * oi.unit_price) AS total_spent
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN customers c ON o.customer_id = c.customer_id
+GROUP BY o.customer_id;
+    
